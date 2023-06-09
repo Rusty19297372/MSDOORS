edfd947bfa920d060b85159f3d31a2bbf1c6a07a
Rusty19297372 <136118534+Rusty19297372@users.noreply.github.com>
2023-06-09 14:15:12 -0500

    Update MSDOORS.lua

--- MSDOORS.lua
+++ MSDOORS.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/mstudio45/MSDOORS/main/MSHUB_Loader.lua"),true))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/mstudio45/MSDOORS/main/MSHUB_Loader.lua"),true))()
